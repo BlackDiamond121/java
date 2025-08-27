961495441672303991356641a2fa1ca616a9f943
|_| <yong090617qp@gmail.com>
2025-08-27 15:18:26 +0900

    2025 08 27

---
+++ chap02/src/sec03/exam03/ByteOperationExample.java
@@ -0,0 +1,17 @@
+package sec03.exam03;
+
+public class ByteOperationExample {
+
+	public static void main(String[] args) {
+		byte result1 = 10 + 20;
+		System.out.println(result1);
+		
+		byte x = 20;
+		byte y = 30;
+		// byte result = x+ y; // 에러
+		int result2 = x + y;
+		System.out.println(result2);
+
+	}
+
+}
---
+++ chap03/src/sec02/exam08/ComperatorExample.java
@@ -0,0 +1,19 @@
+package sec02.exam08;
+
+public class ComperatorExample {
+
+	public static void main(String[] args) {
+		int v2 = 1;
+		double v3 = 1.0;
+		System.out.println(v2 == v3); //true
+		
+		double v4 = 0.1;
+		float v5 = 0.1f; // float 0.1는 정확히 0.1이 X, 0.1보다 미세하게 더 큼
+		System.out.println(v4 == v5); //false
+		System.out.println((float)v4 == v5); //true
+		System.out.println((int)(v4*10) == (int)(v5*10)); //true
+		
+
+	}
+
+}
