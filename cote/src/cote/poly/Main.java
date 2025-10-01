41350755b0f0fd05d0efabdbb443da19797da779
|_| <yong090617qp@gmail.com>
2025-10-01 13:31:07 +0900

    02025/10/1

---
+++ cote/src/cote/poly/Main.java
@@ -0,0 +1,16 @@
+package cote.poly;
+
+public class Main {
+	public static void main(String[] args) {
+		Animal[] myAnimals = new Animal[5];
+		myAnimals[0] = new Parrot();
+		myAnimals[1] = new Cat();
+		myAnimals[3] = new Parrot();
+		myAnimals[3] = new Cat();
+		myAnimals[4] = new Parrot();
+		
+		for(Animal a:myAnimals) {
+			System.out.println(a.hello());
+		}
+	}
+}
